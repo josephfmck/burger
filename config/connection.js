40e847abc5282497410a7ebbcaf45d613c39eb50
Joe <josephfmck@gmail.com>
2018-11-17 13:49:34 -0600

    Added gitignore, using jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,8 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+require("dotenv").config();
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Picori235",
-  database: "burgers_db"
-});
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 // Make connection.
 connection.connect(function(err) {
